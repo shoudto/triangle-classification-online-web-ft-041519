ab87c4a416ce1334b865be7f01aecde4846cb181
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 02:53:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,6 +15,9 @@ class Triangle
     #binding.pry
     if @side_1 == 0 && @side_2 == 0 && @side_3 == 0 
       raise TriangleError
+      
+    elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0 
+      raise TriangleError
     
     elsif @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
@@ -28,8 +31,6 @@ class Triangle
      @side_1 == 0 && @side_2 == 0 && @side_3 == 0 
       raise TriangleError
       
-    elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0 
-      raise TriangleError
     end 
   end
   
