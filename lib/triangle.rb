f35f424cb48e1c7cba086dae2be882c535777fde
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 01:28:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
   
   def kind
    # binding.pry 
-    if side_1 == side_2 && side_1 == side_3
+    if (side_1 == side_2) && (side_1 == side_3)
       puts "equal triangle"
       else  
         "you suck"
