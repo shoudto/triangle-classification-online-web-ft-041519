18b8584b880aedfd55dbddbca3e81d00db77b872
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 01:10:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,13 +4,11 @@ class Triangle
   attr_accessor :side_1, :side_2, :side_3
   
   def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3 
+   @sides = [side_1, side_2, side_3]
   end 
   
   def kind
-    #binding.pry 
-    side_1 side_2 side_3
+    binding.pry 
+   
   end 
 end
