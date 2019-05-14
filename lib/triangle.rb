8f06496cfe89c3bfcd1a50eb5da05dbed71f0602
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 02:35:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,15 +22,13 @@ class Triangle
     else @side_1 != @side_2 && @side_2 != @side_3
       :scalene
       
-    elsif 
+    elsif  side_1 == 0 || side_2 == 0 || side_3 == 0 
+      raise TriangleError
+      
+    elsif  side_1 == 0 && side_2 == 0 && side_3 == 0 
+      raise TriangleError 
     end 
   end
-  
-  def validate_triangle 
-    if side_1 == 0 || side_2 == 0 || side_3 == 0 
-      raise TriangleError
-    end 
-  end 
   
   class TriangleError < StandardError
   end 
