bee99e079c2a62e00c43bc32284186ab019a8c45
Oscar Shoudt <shoudto@gmail.com>
2019-05-14 01:38:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
     elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
       :isosceles
       
-    elsif @side_1 !== @side_2 && @side_2 !== @side_3
+    elsif @side_1 != @side_2 && @side_2 != @side_3
       :scalene
     end 
   end 
